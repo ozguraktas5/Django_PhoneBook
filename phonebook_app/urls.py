bfc4aaa3601e05c055cf79519f7df980121b2f23
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2023-09-05 13:55:07 +0300

    05.09.2023

--- phonebook_app/urls.py
+++ phonebook_app/urls.py
@@ -9,12 +9,13 @@ urlpatterns = [
     path("kisiekle/", views.kisi_ekle, name="kisiekle"),
     path("kategoriekle/", views.kategori_ekle,name="kategoriekle"),
     path("kategorilistesi/", views.kategori_listesi, name="kategorilistesi"),
-    path("kategoriduzenle/<int:category_id>/", views.kategori_duzenle, name="kategoriduzenle"),
+    path("kategoriduzenle/<int:category_id>/", views.kategori_duzenle, name='kategoriduzenle'),
     path("kategorisil/<int:category_id>/", views.kategori_sil, name="kategorisil"),
     path("lokasyonekle/<int:person_id>/", views.lokasyon_ekle, name="lokasyonekle"),
     path('editperson/<int:person_id>/', views.edit_person, name='edit_person'),
     path('deleteperson/<int:person_id>/', views.delete_person, name='delete_person'),
     path("upload/", views.upload, name="upload_image"),
+    path("search/", views.search_results, name='search_results'),
     
         
 ]
--- phonebook_app/views.py
+++ phonebook_app/views.py
@@ -30,50 +30,40 @@ def kategori_ekle(request):
         form = CategoryForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect("allpersons")
+        return redirect("kategorilistesi")
     else:
         form = CategoryForm()
     return render(request, "phonebook/kategori_ekle.html", {"form": form})
 
+
 def kategori_listesi(request):
     categories = Category.objects.all()
-    return render(request, "phonebook/kategori_listesi.html", {'categories': categories})
+    return render(
+        request, "phonebook/kategori_listesi.html", {"categories": categories}
+    )
+
 
 def kategori_duzenle(request, category_id):
     category = get_object_or_404(Category, id=category_id)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CategoryForm(request.POST, instance=category)
         if form.is_valid():
             form.save()
             return redirect("kategorilistesi")
     else:
         form = CategoryForm(instance=category)
-    return render(request, 'phonebook/kategori_duzenle.html', {'form': form})
+    return render(request, "phonebook/kategori_duzenle.html", {"form": form})
 
-# def edit_person(request, person_id):
-#     person = get_object_or_404(Person, id=person_id)
-#     if request.method == "POST":
-#         form = PersonForm(request.POST, request.FILES, instance=person)
-#         if form.is_valid():
-#             form.save()
-#             return redirect("allpersons")
-#     else:
-#         form = PersonForm(instance=person)
-#     return render(request, "phonebook/edit_person.html", {"form": form})
 
 def kategori_sil(request, category_id):
-    category = get_object_or_404(Person, id=category_id)
+    category = get_object_or_404(Category, id=category_id)
     category.delete()
     return redirect("kategorilistesi")
-    
 
-# def delete_person(request, person_id):
-#     person = get_object_or_404(Person, id=person_id)
-#     person.delete()
-#     return redirect("allpersons")
+
 def lokasyon_ekle(request, person_id):
     person = Person.objects.get(id=person_id)
-    
+
     if request.method == "POST":
         form = LocationForm(request.POST)
         if form.is_valid():
@@ -81,7 +71,7 @@ def lokasyon_ekle(request, person_id):
             person.location = location
             person.save()
             return redirect("allpersons")
-    
+
     form = LocationForm()
     return render(
         request, "phonebook/lokasyon_ekle.html", {"form": form, "person": person}
@@ -117,7 +107,13 @@ def upload(request):
         form = UploadForm()
     return render(request, "phonebook/upload.html", {"form": form})
 
+
 def some_view(request):
     categories = Category.objects.all()
     context = {"categories": categories}
     return render(request, "../templates/sidebar.html", context)
+
+def search_results(request):
+    query = request.GET.get('q')
+    results = Person.objects.filter(first_name__icontains=query) | Person.objects.filter(last_name__icontains=query)
+    return render(request, 'phonebook/search_results.html', {'results': results, 'query': query})
